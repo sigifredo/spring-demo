de774b48934842195daac1a10c6c186bba7bb166
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2017-07-12 12:03:12 -0500

    Corregimos método de recepción de la petición a la URL addperson.

--- src/main/java/com/nullpoint/demo/controllers/Example3Controller.java
+++ src/main/java/com/nullpoint/demo/controllers/Example3Controller.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
@@ -34,7 +35,7 @@ public class Example3Controller {
 		return FORM_VIEW;
 	}
 
-	@GetMapping("/addperson")
+	@PostMapping("/addperson")
 	public ModelAndView addPerson(@Valid @ModelAttribute("person") Person person, BindingResult bindingResult) {
 		ModelAndView modelAndView = new ModelAndView();
 
