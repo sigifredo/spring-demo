a2b668e181638eeace486d9b55102ac6ba83359b
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2017-07-12 01:59:30 -0500

    ExampleService was created.

---
+++ src/main/java/com/nullpoint/demo/services/ExampleService.java
@@ -0,0 +1,11 @@
+package com.nullpoint.demo.services;
+
+import java.util.List;
+
+import com.nullpoint.demo.models.Person;
+
+public interface ExampleService {
+
+	public abstract List<Person> getListPeople();
+
+}
---
+++ src/main/java/impl/ExampleServiceImpl.java
@@ -0,0 +1,26 @@
+package impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.nullpoint.demo.models.Person;
+import com.nullpoint.demo.services.ExampleService;
+
+@Service("exampleService")
+public class ExampleServiceImpl implements ExampleService {
+
+	@Override
+	public List<Person> getListPeople() {
+		List<Person> people = new ArrayList<Person>();
+
+		people.add(new Person("Jesús", 33));
+		people.add(new Person("Raul", 27));
+		people.add(new Person("Pablo", 40));
+
+		// TODO Auto-generated method stub
+		return people;
+	}
+
+}
