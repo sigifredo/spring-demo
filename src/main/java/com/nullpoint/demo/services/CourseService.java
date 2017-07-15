b797399e40805d0015c686ab7388138eab3fd0c0
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2017-07-15 01:13:26 -0500

    Creamos controladores para utilizar el servicio "CourseService".

--- src/main/java/com/nullpoint/demo/services/CourseService.java
+++ src/main/java/com/nullpoint/demo/services/CourseService.java
@@ -1,5 +1,15 @@
 package com.nullpoint.demo.services;
 
-public interface CourseService {
+import java.util.List;
 
+import com.nullpoint.demo.entity.Course;
+
+public interface CourseService {
+	public abstract List<Course> listAllCourses();
+
+	public abstract Course addCourse(Course course);
+
+	public abstract int removeCourse(int id);
+
+	public abstract Course updateCourse(Course course);
 }
