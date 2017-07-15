0d80a364c13bd0d91ab7c1d15c641aafb7cc6ecc
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2017-07-15 16:53:12 -0500

    Simulamos el login.

---
+++ src/main/java/com/nullpoint/demo/controller/LoginController.java
@@ -0,0 +1,49 @@
+package com.nullpoint.demo.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.nullpoint.demo.model.UserCredential;
+
+@Controller
+public class LoginController {
+
+	private static final Log LOGGER = LogFactory.getLog(LoginController.class);
+
+	@GetMapping("/")
+	public String redirectToLogin() {
+		LOGGER.info("METHOD: redirectToLogin()");
+		return "redirect:/login";
+	}
+
+	@GetMapping("/login")
+	public String showLoginForm(Model model, @RequestParam(name = "error", required = false) String error,
+			@RequestParam(name = "logout", required = false) String logout) {
+		LOGGER.info("METHOD: showLoginForm() -- PARAMS: error='" + error + "', logout='" + logout + "'");
+
+		model.addAttribute("error", error);
+		model.addAttribute("logout", logout);
+		model.addAttribute("userCredentials", new UserCredential());
+		return "login";
+	}
+
+	@PostMapping("/logincheck")
+	public String loginCheck(@ModelAttribute(name = "userCredentials") UserCredential userCredential) {
+		LOGGER.info("METHOD: loginCheck() -- PARAMS: userCredential='" + userCredential + "'");
+
+		if (userCredential.getUserName().equals("user") && userCredential.getPassword().equals("user")) {
+			LOGGER.info("Returning to contacts view");
+			return "contacts";
+		} else {
+			LOGGER.info("Returning to login?error");
+			return "redirect:/login?error";
+		}
+	}
+
+}
---
+++ src/main/java/com/nullpoint/demo/model/UserCredential.java
@@ -0,0 +1,39 @@
+package com.nullpoint.demo.model;
+
+public class UserCredential {
+
+	private String userName;
+	private String password;
+
+	public UserCredential() {
+		this.userName = "";
+		this.password = "";
+	}
+
+	public UserCredential(String userName, String password) {
+		this.userName = userName;
+		this.password = password;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "UserCredential [userName=" + userName + ", password=" + password + "]";
+	}
+
+}
