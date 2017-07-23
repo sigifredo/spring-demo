915ff0363e4a7cd8033adfb40234d1473752f1af
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2017-07-22 19:10:00 -0500

    Agregamos etiqueta PreAuthorize.

--- src/main/java/com/nullpoint/demo/controller/ContactController.java
+++ src/main/java/com/nullpoint/demo/controller/ContactController.java
@@ -4,6 +4,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,6 +35,7 @@ public class ContactController {
 		return "redirect:/contacts/showcontacts";
 	}
 
+	@PreAuthorize("hasRole('ROLE_USER')")
 	@GetMapping("/contactform")
 	public String redirectContactForm(@RequestParam(name = "id", required = false) Integer id, Model model) {
 		ContactModel contactModel = null;
@@ -61,7 +65,9 @@ public class ContactController {
 	@GetMapping("/showcontacts")
 	public ModelAndView showContact() {
 		ModelAndView modelAndView = new ModelAndView(ViewConstant.CONTACTS);
+		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
+		modelAndView.addObject("username", user.getUsername());
 		modelAndView.addObject("contacts", contactService.listAllContacts());
 
 		return modelAndView;
